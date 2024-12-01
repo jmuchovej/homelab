1edb697d8ad3cfe0a66a0a0b39eba1bdf568b127
John Muchovej <jmuchovej@users.noreply.github.com>
2024-11-30 23:33:17 -0500

    fix(common/zerotier): typo
    Signed-off-by: John Muchovej <jmuchovej@users.noreply.github.com>

--- common/modules/zerotier.nix
+++ common/modules/zerotier.nix
@@ -1,10 +1,9 @@
 { config, ... }: {
-  sops.secrets."/var/lib/zerotier-one/networks.d/${}"
   services.zerotierone = {
     enable        = true;
     joinNetworks  = [
       "48d6023c4695358b"  # Homelab
       # "!!ZEROTIER_HOMELAB!!"
-    ]
+    ];
   };
 }
