4ac6285107eff0c060b413b943a3ec49f783b952
John Muchovej <jmuchovej@users.noreply.github.com>
2024-12-30 21:03:27 -0500

    feat(snowfall): updating `flake` to use `snowfall`
    Signed-off-by: John Muchovej <jmuchovej@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -8,22 +8,34 @@
     # Nix & NixOS
     nix.url = "github:NixOS/nix/2.25-maintenance";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs?ref=master";
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/release-24.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     # Topology
     topology.url = "github:oddlama/nix-topology";
 
     # Nix Darwin
-    darwin.url = "github:LnL7/nix-darwin";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    # darwin.url = "github:LnL7/nix-darwin";
+    darwin.url = "github:khaneliman/nix-darwin/cherry-picks";
+    darwin.inputs.nixpkgs.follows = "nixpkgs-unstable";
+
+    # Install Mac Apps in a Spotlight-discovery way.
+    mac-app-util.url = "github:hraban/mac-app-util";
+
+    # System images/artifacts builder
+    # nixos-generators = {
+    #   url = "github:nix-community/nixos-generators";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
 
     # Nix User Repository
     nur.url = "github:nix-community/NUR";
 
     # Home Manager
-    # home-manager.url = "github:nix-community/home-manager?ref=release-24.05";
-    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # SOPS
     sops-nix.url = "github:Mic92/sops-nix";
@@ -37,18 +49,16 @@
   };
 
   # outputs = { ... } @ args: import ./flake-outputs.nix args;
-  outputs = inputs:
-    inputs.snowfall-lib.mkLib {
+  outputs = {
+    snowfall-lib, nur, sops-nix, home-manager, mac-app-util,
+    ...
+  } @ inputs: let
+    lib = snowfall-lib.mkLib {
       inherit inputs;
-
-      channels-config = {
-        allowUnfree = true;
-      };
-
       src = ./.;
 
       snowfall = {
-        metadata = "rebellion";
+        # metadata = "rebellion";
         namespace = "rebellion";
 
         meta = {
@@ -56,10 +66,31 @@
           title = "The Rebellion";
         };
       };
+    };
+  in lib.mkFlake {
+      channels-config = {
+        allowUnfree = true;
+      };
+
+      overlays = [
+        nur.overlays.default
+      ];
+
+      homes.modules = with inputs; [
+        mac-app-util.homeManagerModules.default
+        sops-nix.homeManagerModules.sops
+      ];
 
       systems.modules = {
-        darwin  = with inputs; [ sops-nix.nixosModules.sops ];
-        nixos   = with inputs; [ sops-nix.nixosModules.sops ];
+        darwin  = with inputs; [
+          mac-app-util.darwinModules.default
+          home-manager.darwinModules.home-manager
+          sops-nix.darwinModules.sops
+        ];
+        nixos   = with inputs; [
+          home-manager.nixosModules.home-manager
+          sops-nix.nixosModules.sops
+        ];
       };
     };
 }
