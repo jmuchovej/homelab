c74e4d947694c9fc415c8a78067a0e0b5b62775b
John Muchovej <jmuchovej@users.noreply.github.com>
2025-01-06 15:14:38 -0500

    nixos/programs/shells/zsh: init
    Signed-off-by: John Muchovej <jmuchovej@users.noreply.github.com>

---
+++ modules/nixos/programs/shells/zsh/default.nix
@@ -0,0 +1,33 @@
+{
+  config,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}: let
+  inherit (lib) mkEnableOption mkIf;
+
+  cfg = config.${namespace}.programs.shells.zsh;
+in {
+  options.${namespace}.programs.shells.zsh = {
+    enable = mkEnableOption "`zsh`";
+  };
+
+  config = mkIf cfg.enable {
+    programs.zsh = {
+      enable = true;
+
+      autosuggestions.enable = true;
+      enableCompletion = true;
+    };
+
+    environment.sessionVariables = {
+      LC_ALL = "en_US.UTF-8";
+      KEYTIMEOUT = 0;
+    };
+
+    environment.systemPackages = with pkgs; [
+      nix-zsh-completions
+    ];
+  };
+}
