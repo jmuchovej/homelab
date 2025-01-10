6f4e3ca044e2477c447ffdf4f2abca220679fdce
John Muchovej <jmuchovej@users.noreply.github.com>
2025-01-09 22:36:19 -0500

    homes/linux/lab: see if adding `lab` will ease setup?
    Signed-off-by: John Muchovej <jmuchovej@users.noreply.github.com>

---
+++ homes/x86_64-linux/lab/default.nix
@@ -0,0 +1,33 @@
+{
+  config,
+  lib,
+  namespace,
+  ...
+}: let
+  inherit (lib.${namespace}) enabled;
+in {
+  rebellion = {
+    user = enabled // {
+      inherit (config.snowfallorg.user) name;
+    };
+
+    programs = {
+      tools = {
+        ssh = enabled // {
+          authorized-keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID3FPLe1ZXSk7KBgSkJud2hlvUAGF5m57g2Pqpccy5SO"
+          ];
+        };
+      };
+    };
+
+    suites = {
+      common = enabled;
+      development = enabled;
+    };
+  };
+
+  # ======================== DO NOT CHANGE THIS ========================
+  home.stateVersion = "24.11";
+  # ======================== DO NOT CHANGE THIS ========================
+}
