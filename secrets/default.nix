31dc40ab6738939dfa2c801171a36768afcc240f
John Muchovej <jmuchovej@users.noreply.github.com>
2025-01-09 22:39:28 -0500

    chore: cleanup unnecessary files
    Signed-off-by: John Muchovej <jmuchovej@users.noreply.github.com>

--- secrets/default.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, lib, ...  }: {
-  imports = [ <sops-nix/modules/sops> ];
-
-  sops.secrets.shared = {
-    sopsFile   = ./secrets.sops.yaml;
-    format     = "yaml";
-    parseValue = true;
-  };
-}
