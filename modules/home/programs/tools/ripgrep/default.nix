c16f0dd670edfd6732740c1d25f14b1766bc0b2e
John Muchovej <jmuchovej@users.noreply.github.com>
2025-01-09 22:57:17 -0500

    feat(home/.../rg): add ripgrep
    Signed-off-by: John Muchovej <jmuchovej@users.noreply.github.com>

---
+++ modules/home/programs/tools/ripgrep/default.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  lib,
+  pkgs,
+  namespace,
+  ...
+}: let
+  inherit (lib) mkIf mkEnableOption;
+
+  cfg = config.${namespace}.programs.tools.ripgrep;
+in {
+  options.${namespace}.programs.tools.ripgrep = {
+    enable = mkEnableOption "ripgrep";
+  };
+
+  config = mkIf cfg.enable {
+    programs.ripgrep = {
+      enable = true;
+      arguments = [
+        # Avoid dumping long lines to shell
+        "--max-columns=80"
+        "--max-columns-preview"
+        # Search hidden files
+        "--hidden"
+        # Ignore casing
+        "--smart-case"
+        # Follow symlinks while searching
+        "--follow"
+      ];
+    };
+  };
+}
