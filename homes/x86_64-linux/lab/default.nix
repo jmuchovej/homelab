650211af09b1bc60f3f820d8ac8ac0da85b9e0ca
John Muchovej <jmuchovej@users.noreply.github.com>
2025-04-22 18:26:14 +0000

    refactor: flatten tree; rm `enabled` on enable-by-default tools
    Signed-off-by: John Muchovej <jmuchovej@users.noreply.github.com>

--- homes/x86_64-linux/lab/default.nix
+++ homes/x86_64-linux/lab/default.nix
@@ -7,29 +7,23 @@
   inherit (lib.${namespace}) enabled;
 in {
   rebellion = {
-    user = enabled // {
+    user = {
       inherit (config.snowfallorg.user) name;
     };
 
     nix = enabled;
 
-    programs = {
-      tools = {
-        ssh = enabled // {
-          authorized-keys = [
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID3FPLe1ZXSk7KBgSkJud2hlvUAGF5m57g2Pqpccy5SO"
-          ];
-        };
-      };
+    ssh = {
+      authorized-keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID3FPLe1ZXSk7KBgSkJud2hlvUAGF5m57g2Pqpccy5SO"
+      ];
     };
 
-    suites = {
-      common = enabled;
-      development = enabled;
-    };
+    development = enabled;
   };
 
   # ======================== DO NOT CHANGE THIS ========================
   home.stateVersion = "24.11";
   # ======================== DO NOT CHANGE THIS ========================
 }
+
