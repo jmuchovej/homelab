c1ee82e76e7724dd3f40d0da24a66dbd2ea1c14b
John Muchovej <jmuchovej@users.noreply.github.com>
2025-12-14 11:47:16 -0500

    nixos/services/syncthing: rename `-network` to be a `.part`
    Signed-off-by: John Muchovej <jmuchovej@users.noreply.github.com>

--- modules/nixos/services/syncthing.nix
+++ modules/nixos/services/syncthing.nix
@@ -1,28 +1,18 @@
 {
-  # Snowfall Lib provides a customized `lib` instance with access to your flake's library
-  # as well as the libraries available from your flake's inputs.
   lib,
-  # An instance of `pkgs` with your overlays and packages applied is also available.
-  # You also have access to your flake's inputs.
-  # Additional metadata is provided by Snowfall Lib.
-  namespace, # The namespace used for your flake, defaulting to "internal" if not set. # The system architecture for this host (eg. `x86_64-linux`). # The Snowfall Lib target for this system (eg. `x86_64-iso`). # A normalized name for the system target (eg. `iso`). # A boolean to determine whether this system is a virtual target using nixos-generators. # An attribute map of your defined hosts.
-  host,
-  # All other arguments come from the system system.
   config,
-  options,
   ...
 }:
 let
   inherit (lib)
     mkIf
     mkEnableOption
-    types
     ;
   inherit (lib.attrsets) filterAttrs mapAttrs attrNames;
 
   cfg = config.rebellion.services.syncthing;
 
-  syncthing-network = import ./syncthing-network.nix;
+  syncthing-network = import ./syncthing-network.part.nix;
 
   target-devices = filterAttrs (hostname: _runtimeID: hostname != host) syncthing-network.devices;
   devices = mapAttrs (_hostname: runtimeID: {
@@ -58,10 +48,10 @@ in
     networking.firewall.allowedTCPPorts = [ 8384 ];
 
     sops.secrets."syncthing/key" = {
-        owner = config.services.syncthing.user;
+      owner = config.services.syncthing.user;
     };
     sops.secrets."syncthing/cert" = {
-        owner = config.services.syncthing.user;
+      owner = config.services.syncthing.user;
     };
 
     services.syncthing = {
