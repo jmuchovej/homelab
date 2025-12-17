578f9bd93dbe10e91a30f498e726d0bd29464823
John Muchovej <jmuchovej@users.noreply.github.com>
2025-12-17 10:42:20 -0500

    home/desktop: refactor to use `mk-module`
    Signed-off-by: John Muchovej <jmuchovej@users.noreply.github.com>

--- modules/home/desktop.nix
+++ modules/home/desktop.nix
@@ -1,17 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  namespace,
-  ...
-}: let
-  inherit (lib) mkIf mkDefault mkEnableOption;
-  inherit (lib.rebellion) enabled;
-
-  cfg = config.rebellion.desktop;
-in {
-  # This is almost exclusively for hierarchy. Nothing should really go here!
-  options.rebellion.desktop = {
-    enable = mkEnableOption "desktop configuration for home-manager";
-  };
+{ lib, ... }@args:
+lib.rebellion.mk-module args {
+  name = "desktop";
 }
