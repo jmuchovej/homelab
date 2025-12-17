bc6f91762c589f2abb7ef2d7c03b6b71e37154c8
John Muchovej <jmuchovej@users.noreply.github.com>
2025-12-16 20:28:07 -0500

    macos/system/nix: migrate to more centralized `nix` config
    Signed-off-by: John Muchovej <jmuchovej@users.noreply.github.com>

--- modules/macos/system/nix.nix
+++ modules/macos/system/nix.nix
@@ -8,24 +8,63 @@ let
   inherit (lib.rebellion) get-file;
 
   cfg = config.rebellion.system.nix;
-  #! https://github.com/LnL7/nix-darwin/issues/852
-  #! According to ️^, the using `nix.linux-builder` with `nix*-unstable` doesn't work...
-  # linux-builder-package = inputs.nixpkgs-stable.legacyPackages.${system}.darwin.linux-builder;
 in
 {
   imports = [ (get-file "modules/common/system/nix.nix") ];
 
   config = mkIf cfg.enable {
     nix = {
-      # Options that aren't supported through nix-darwin
-      extraOptions = ''
-        # bail early on missing cache hits
-        connect-timeout = 10
-        keep-going = true
-      '';
+      # Enable nix-darwin to manage nix configuration
+      # This will write settings to /etc/nix/nix.conf or nix.custom.conf
+      # depending on the Determinate Nix installation
+      enable = true;
 
-      # gc.user = config.rebellion.user.name;
-      # optimise.user = config.rebellion.user.name;
+      # Additional settings specific to macOS
+      # These will be written to the configuration file
+      settings = {
+        # macOS-specific optimizations
+        max-jobs = "auto";
+        cores = 0; # Use all available cores
+
+        extra-sandbox-paths = [
+          "/System/Library/Frameworks"
+          "/System/Library/PrivateFrameworks"
+          "/usr/lib"
+
+          "/private/tmp"
+          "/private/var/tmp"
+          "/usr/bin/env"
+        ];
+
+        # Bail early on missing cache hits
+        connect-timeout = 10;
+      };
+
+      # Extra options that might not be supported through settings
+      # extraOptions = ''
+      #   # Bail early on missing cache hits
+      #   connect-timeout = 10
+      # '';
+
+      # Configure garbage collection for the user
+      gc = {
+        automatic = true;
+        interval = {
+          Weekday = 0;
+          Hour = 0;
+          Minute = 0;
+        };
+        options = "--delete-older-than 7d";
+      };
+
+      # Configure store optimization
+      optimise.automatic = true;
     };
+
+    # Note: linux-builder configuration can be added here if needed
+    # nix.linux-builder = {
+    #   enable = true;
+    #   package = pkgs.darwin.linux-builder;
+    # };
   };
 }
