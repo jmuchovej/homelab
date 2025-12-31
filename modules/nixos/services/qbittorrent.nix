d62384834105a9eb1bd263db3fccd4ab3b006305
John Muchovej <jmuchovej@users.noreply.github.com>
2025-12-31 14:09:43 -0500

    nixos/services/qbittorrent: set QBIT user/pass, add pvpn
    Signed-off-by: John Muchovej <jmuchovej@users.noreply.github.com>

--- modules/nixos/services/qbittorrent.nix
+++ modules/nixos/services/qbittorrent.nix
@@ -10,12 +10,26 @@ lib.rebellion.mk-module args {
       ...
     }:
     let
-      inherit (lib.rebellion) merge-attrs;
-      inherit (lib.rebellion.network) mk-traefik-service with-consul;
+      inherit (lib) getExe';
+      inherit (lib.rebellion) get-file;
+
+      usr = config.services.qbittorrent.user;
+      grp = config.services.qbittorrent.group;
     in
     lib.mkMerge [
       {
         rebellion.services.proton-vpn.enable = true;
+
+        sops.secrets."qbittorrent/username" = {
+          sopsFile = get-file "secrets/secrets.sops.yaml";
+          owner = usr;
+          group = grp;
+        };
+        sops.secrets."qbittorrent/password" = {
+          sopsFile = get-file "secrets/secrets.sops.yaml";
+          owner = usr;
+          group = grp;
+        };
 
         services.qbittorrent = {
           enable = true;
@@ -31,7 +45,51 @@ lib.rebellion.mk-module args {
               Interface = "proton0";
               InterfaceName = "proton0";
             };
+            LegalNotice.Accepted = true;
           };
+        };
+
+        systemd.services.qbittorrent = {
+          serviceConfig.SupplementaryGroups = [ "proton" ];
+
+          # Inject secrets into qBittorrent config at runtime
+          preStart =
+            let
+              crudini = getExe' pkgs.crudini "crudini";
+              python = getExe' pkgs.python3 "python";
+              profile-dir = config.services.qbittorrent.profileDir;
+            in
+            ''
+              # Read secrets from sops-nix
+              USERNAME=$(cat ${config.sops.secrets."qbittorrent/username".path})
+              PASSWORD=$(cat ${config.sops.secrets."qbittorrent/password".path})
+
+              # Generate PBKDF2 hash for qBittorrent
+              # Format: @ByteArray(salt:hash)
+              HASH=$(${python} -c "
+              import hashlib
+              import os
+              import base64
+
+              password = '$PASSWORD'
+              salt = os.urandom(16)
+              iterations = 100_000
+              algorithm = 'sha512'
+
+              dk = hashlib.pbkdf2_hmac(algorithm, password.encode(), salt, iterations)
+
+              encoded_salt = base64.b64encode(salt).decode()
+              encoded_hash = base64.b64encode(dk).decode()
+
+              print(f'@ByteArray({encoded_salt}:{encoded_hash})')
+              ")
+
+              CONFIG_FILE="${profile-dir}/qBittorrent/config/qBittorrent.conf"
+
+              # Update config file using crudini
+              ${crudini} --set "$CONFIG_FILE" "Preferences" "WebUI\\Username" "$USERNAME"
+              ${crudini} --set "$CONFIG_FILE" "Preferences" "WebUI\\Password_PBKDF2" "\"$HASH\""
+            '';
         };
       }
 
