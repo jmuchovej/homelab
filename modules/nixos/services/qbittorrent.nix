7049b3282a3615c95745749671572c40fbc9384f
John Muchovej <jmuchovej@users.noreply.github.com>
2026-01-10 14:51:55 -0500

    nixos/services/qbittorrent: setup authentik
    Signed-off-by: John Muchovej <jmuchovej@users.noreply.github.com>

--- modules/nixos/services/qbittorrent.nix
+++ modules/nixos/services/qbittorrent.nix
@@ -10,27 +10,9 @@ lib.rebellion.mk-module args {
       datacenter,
       ...
     }:
-    let
-      inherit (lib) getExe';
-      inherit (lib.rebellion) get-file;
-
-      usr = config.services.qbittorrent.user;
-      grp = config.services.qbittorrent.group;
-    in
     lib.mkMerge [
       {
         rebellion.services.proton-vpn.enable = true;
-
-        sops.secrets."qbittorrent/username" = {
-          sopsFile = get-file "secrets/secrets.sops.yaml";
-          owner = usr;
-          group = grp;
-        };
-        sops.secrets."qbittorrent/password" = {
-          sopsFile = get-file "secrets/secrets.sops.yaml";
-          owner = usr;
-          group = grp;
-        };
 
         services.qbittorrent = {
           enable = true;
@@ -41,6 +23,12 @@ lib.rebellion.mk-module args {
               Address = "*";
               AlternativeUIEnabled = true;
               RootFolder = "${pkgs.vuetorrent}/share/vuetorrent";
+              BypassAuthSubnetWhitelist = true;
+              AuthSubnetWhitelist = "10.69.0.0/16";
+              # NOTE: added b/c SOPS templates + crudini doesn't work since the
+              #   qBittorrent.conf is read-only... >.>
+              Username = "servarr";
+              Password_PBKDF2 = "@ByteArray(hLn8qRJjzDennq46o+5UsQ==:DR/FTvFcELQeRuAqNA0rpWTzV6v+oFEvcDGw4TBo2YXfjr8M1TrcXMNTXJr/zqQuIICyLS/aTjOq4crRHOMH9Q==)";
             };
             BitTorrent.Session = {
               Interface = "proton0";
@@ -50,58 +38,18 @@ lib.rebellion.mk-module args {
           };
         };
 
-        systemd.services.qbittorrent =
-          let
-            crudini = getExe' pkgs.crudini "crudini";
-            python = getExe' pkgs.python3 "python";
-            qb = config.services.qbittorrent;
-            # Inject secrets into qBittorrent config at runtime
-            pre-start = pkgs.writeShellScript "qbittorrent-inject-secrets" ''
-              # Read secrets from sops-nix
-              USERNAME=$(cat ${config.sops.secrets."qbittorrent/username".path})
-              PASSWORD=$(cat ${config.sops.secrets."qbittorrent/password".path})
-
-              # Generate PBKDF2 hash for qBittorrent
-              # Format: @ByteArray(salt:hash)
-              HASH=$(${python} -c "
-              import hashlib
-              import os
-              import base64
-
-              password = '$PASSWORD'
-              salt = os.urandom(16)
-              iterations = 100_000
-              algorithm = 'sha512'
-
-              dk = hashlib.pbkdf2_hmac(algorithm, password.encode(), salt, iterations)
-
-              encoded_salt = base64.b64encode(salt).decode()
-              encoded_hash = base64.b64encode(dk).decode()
-
-              print(f'@ByteArray({encoded_salt}:{encoded_hash})')
-              ")
-
-              CONFIG_FILE="${qb.profileDir}/qBittorrent/config/qBittorrent.conf"
-              mkdir -p "$(dirname "$CONFIG_FILE")"
-              touch "$CONFIG_FILE"
-              chown -R ${qb.user}:${qb.group} ${qb.profileDir}
-
-              # Update config file using crudini
-              ${crudini} --set "$CONFIG_FILE" "Preferences" "WebUI\\Username" "$USERNAME"
-              ${crudini} --set "$CONFIG_FILE" "Preferences" "WebUI\\Password_PBKDF2" "\"$HASH\""
-              chown -R ${qb.user}:${qb.group} "$CONFIG_FILE"
-            '';
-          in
-          {
-            serviceConfig.SupplementaryGroups = [ "proton" ];
-            serviceConfig.ExecStartPre = lib.mkAfter [ "+${pre-start} " ];
-          };
+        systemd.services.qbittorrent.serviceConfig.SupplementaryGroups = [ "proton" ];
       }
 
       (
         let
           inherit (lib.rebellion) merge-attrs;
-          inherit (lib.rebellion.network) with-consul mk-traefik-service mk-healthcheck;
+          inherit (lib.rebellion.network)
+            with-consul
+            mk-traefik-service
+            mk-healthcheck
+            mk-authentik
+            ;
           service = merge-attrs [
             (mk-traefik-service {
               inherit hostname datacenter;
@@ -116,8 +64,24 @@ lib.rebellion.mk-module args {
           healthcheck = mk-healthcheck service {
             route = "/"; # Root path doesn't require auth
           };
+          authentik-tags = mk-authentik service {
+            name = "qBittorrent";
+            type = "proxy";
+            group = "Media Management";
+            access = [ "media-managers" ];
+            icon = "qbittorrent";
+            skip-paths = "/api/*";
+            basic-auth.username = "servarr-username";
+            basic-auth.password = "servarr-password";
+          };
         in
-        with-consul config (service // { checks = [ healthcheck ]; })
+        with-consul config (
+          service
+          // {
+            checks = [ healthcheck ];
+            tags = authentik-tags;
+          }
+        )
       )
     ];
 }
