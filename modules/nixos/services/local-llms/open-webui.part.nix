ee58af91105960f3d34c6076945f28ffca9a8bf1
John Muchovej <jmuchovej@users.noreply.github.com>
2026-01-15 13:03:13 -0500

    nixos/services/local-llms: update oauth specs for open-webui
    Signed-off-by: John Muchovej <jmuchovej@users.noreply.github.com>

--- modules/nixos/services/local-llms/open-webui.part.nix
+++ modules/nixos/services/local-llms/open-webui.part.nix
@@ -28,9 +28,9 @@ lib.mkMerge [
       in
       ''
         WEBUI_SECRET_KEY=${config.sops.placeholder."open-webui/secret-key"}
-        OAUTH_CLIENT_ID="${client-id}"
-        OAUTH_CLIENT_SECRET="${config.sops.placeholder."open-webui/client-secret"}"
-        OPENID_PROVIDER_URL="${mk-openid-url client-id datacenter}"
+        OAUTH_CLIENT_ID=${client-id}
+        OAUTH_CLIENT_SECRET=${config.sops.placeholder."open-webui/client-secret"}
+        OPENID_PROVIDER_URL=${mk-openid-url client-id datacenter}
       '';
     services.open-webui =
       let
@@ -41,6 +41,10 @@ lib.mkMerge [
         host = ollama.host;
         port = ollama.port + 1;
         environment = {
+          WEBUI_URL = "https://chat.${datacenter}.jm0.io";
+          SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt";
+          REQUESTS_CA_BUNDLE = "/etc/ssl/certs/ca-certificates.crt";
+
           ANONYMIZED_TELEMETRY = "False";
           DO_NOT_TRACK = "True";
           SCARF_NO_ANALYTICS = "True";
@@ -73,12 +77,16 @@ lib.mkMerge [
           RAG_EMBEDDING_ENGINE = "ollama";
 
           # OAuth: https://docs.openwebui.com/getting-started/env-configuration/#openid-oidc
+          ENABLE_LOGIN_FORM = "False";
+          ENABLE_PASSWORD_AUTH = "False";
+          ENABLE_OAUTH_SIGNUP = "True";
           OAUTH_PROVIDER_NAME = "The Rebellion";
           OAUTH_SCOPES = "openid email profile";
           OAUTH_PICTURE_CLAIM = "avatar";
           OAUTH_GROUP_CLAIM = "groups";
           ENABLE_OAUTH_GROUP_MANAGEMENT = "True";
           ENABLE_OAUTH_GROUP_CREATION = "True";
+          OAUTH_UPDATE_PICTURE_ON_LOGIN = "True";
           OAUTH_ALLOWED_ROLES = "compute,compute-manager";
           OAUTH_ADMIN_ROLES = "compute-manager";
           OAUTH_ALLOWED_DOMAINS = "*.${datacenter}.jm0.io,*.jm0.io,jm0.io";
