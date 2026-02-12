9ec9b5b653767475e80064e58d46078e4092f374
John Muchovej <jmuchovej@users.noreply.github.com>
2026-02-11 20:40:45 -0500

    nixos/suites/server: attempt to resolve `lab/password` access
    Signed-off-by: John Muchovej <jmuchovej@users.noreply.github.com>

--- modules/macos/system/nix.nix
+++ modules/macos/system/nix.nix
@@ -3,47 +3,45 @@ lib.rebellion.mk-module args {
   name = "system.nix";
   always-active = true;
   imports = [ (lib.rebellion.get-file "modules/common/system/nix.nix") ];
-  config =
-    { lib, ... }:
-    {
-      nix = {
-        # Enable nix-darwin to manage nix configuration
-        # This will write settings to /etc/nix/nix.conf or nix.custom.conf
-        # depending on the Determinate Nix installation
-        enable = true;
+  config = _: {
+    nix = {
+      # Enable nix-darwin to manage nix configuration
+      # This will write settings to /etc/nix/nix.conf or nix.custom.conf
+      # depending on the Determinate Nix installation
+      enable = true;
 
-        # Additional settings specific to macOS
-        settings = {
-          max-jobs = "auto";
-          cores = 0; # Use all available cores
+      # Additional settings specific to macOS
+      settings = {
+        max-jobs = "auto";
+        cores = 0; # Use all available cores
 
-          extra-sandbox-paths = [
-            "/System/Library/Frameworks"
-            "/System/Library/PrivateFrameworks"
-            "/usr/lib"
+        extra-sandbox-paths = [
+          "/System/Library/Frameworks"
+          "/System/Library/PrivateFrameworks"
+          "/usr/lib"
 
-            "/private/tmp"
-            "/private/var/tmp"
-            "/usr/bin/env"
-          ];
+          "/private/tmp"
+          "/private/var/tmp"
+          "/usr/bin/env"
+        ];
 
-          # Bail early on missing cache hits
-          connect-timeout = 10;
-        };
-
-        # Configure garbage collection for the user
-        gc = {
-          automatic = true;
-          interval = {
-            Weekday = 0;
-            Hour = 0;
-            Minute = 0;
-          };
-          options = "--delete-older-than 7d";
-        };
-
-        # Configure store optimization
-        optimise.automatic = true;
+        # Bail early on missing cache hits
+        connect-timeout = 10;
       };
+
+      # Configure garbage collection for the user
+      gc = {
+        automatic = true;
+        interval = {
+          Weekday = 0;
+          Hour = 0;
+          Minute = 0;
+        };
+        options = "--delete-older-than 7d";
+      };
+
+      # Configure store optimization
+      optimise.automatic = true;
     };
+  };
 }
