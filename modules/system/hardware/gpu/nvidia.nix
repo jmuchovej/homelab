899e02c65c78931b429b3eb7b760a0974e3f4029
John Muchovej <jmuchovej@users.noreply.github.com>
2026-04-11 21:03:07 -0400

    chore(treefmt): apply formatter sweep

--- modules/system/hardware/gpu/nvidia.nix
+++ modules/system/hardware/gpu/nvidia.nix
@@ -1,67 +1,73 @@
-{ den, ... }: {
+{ den, ... }:
+{
   rbn.system._.hardware._.gpu._.nvidia = {
-    includes = [ (den.provides.unfree [ "nvidia-x11" "nvidia-settings" ]) ];
+    includes = [
+      (den.provides.unfree [
+        "nvidia-x11"
+        "nvidia-settings"
+      ])
+    ];
 
     nixos =
-    {
-      lib,
-      pkgs,
-      config,
-      ...
-    }:
-    let
-      # use the latest possible nvidia package
-      nvStable = config.boot.kernelPackages.nvidiaPackages.stable.version;
-      nvBeta = config.boot.kernelPackages.nvidiaPackages.beta.version;
+      {
+        lib,
+        pkgs,
+        config,
+        ...
+      }:
+      let
+        # use the latest possible nvidia package
+        nvStable = config.boot.kernelPackages.nvidiaPackages.stable.version;
+        nvBeta = config.boot.kernelPackages.nvidiaPackages.beta.version;
 
-      nvidiaPackage =
-        if (lib.versionOlder nvBeta nvStable) then
-          config.boot.kernelPackages.nvidiaPackages.stable
-        else
-          config.boot.kernelPackages.nvidiaPackages.beta;
-    in
-    {
-      boot.blacklistedKernelModules = [
-        "nouveau"
-        "nvidiafb"
-      ];
+        nvidiaPackage =
+          if (lib.versionOlder nvBeta nvStable) then
+            config.boot.kernelPackages.nvidiaPackages.stable
+          else
+            config.boot.kernelPackages.nvidiaPackages.beta;
+      in
+      {
+        boot.blacklistedKernelModules = [
+          "nouveau"
+          "nvidiafb"
+        ];
 
-      environment = {
-        variables = {
-          CUDA_CACHE_PATH = "$XDG_CACHE_HOME/nv";
+        environment = {
+          variables = {
+            CUDA_CACHE_PATH = "$XDG_CACHE_HOME/nv";
+          };
+
+          shellAliases = {
+            nvidia-settings = "nvidia-settings --config='$XDG_CONFIG_HOME'/nvidia/settings";
+          };
+
+          systemPackages = with pkgs; [
+            nvfancontrol
+          ];
         };
 
-        shellAliases = {
-          nvidia-settings = "nvidia-settings --config='$XDG_CONFIG_HOME'/nvidia/settings";
+        nixpkgs.config.nvidia.acceptLicense = true;
+
+        nix.settings = {
+          substituters = [
+            "https://cuda-maintainers.cachix.org"
+          ];
+          trusted-public-keys = [
+            "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+          ];
         };
 
-        systemPackages = with pkgs; [
-          nvfancontrol
-        ];
-      };
+        services.xserver.videoDrivers = lib.mkDefault [ "nvidia" ];
 
-      nixpkgs.config.nvidia.acceptLicense = true;
+        hardware = {
+          graphics.enable = true;
 
-      nix.settings = {
-        substituters = [
-          "https://cuda-maintainers.cachix.org"
-        ];
-        trusted-public-keys = [
-          "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-        ];
-      };
-
-      services.xserver.videoDrivers = lib.mkDefault [ "nvidia" ];
-
-      hardware = {
-        graphics.enable = true;
-
-        nvidia = {
-          open = false;
-          package = lib.mkDefault nvidiaPackage;
-          nvidiaSettings = false;
+          nvidia = {
+            open = false;
+            package = lib.mkDefault nvidiaPackage;
+            nvidiaSettings = false;
+          };
         };
       };
-    };
   };
 }
