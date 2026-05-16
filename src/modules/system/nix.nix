ab69c687f14b81639cf9933085c327e56787e164
John Muchovej <jmuchovej@users.noreply.github.com>
2026-05-16 07:56:33 -0400

    feat(nix-builders): add remote builders (and linux VM on macOS)

--- src/modules/system/nix.nix
+++ src/modules/system/nix.nix
@@ -242,6 +242,18 @@
         };
 
         optimise.automatic = true;
+
+        linux-builder = {
+          enable = true;
+          ephemeral = true;
+          maxJobs = 4;
+          config = {
+            virtualisation = {
+              darwin-builder.diskSize = 40 * 1024;
+              darwin-builder.memorySize = 8 * 1024;
+            };
+          };
+        };
       };
     };
   };
