673991d6d720b1e1bae3981b35290f433b08ace3
John Muchovej <jmuchovej@users.noreply.github.com>
2026-05-16 07:56:33 -0400

    feat(da-gr75): add `lab` user

--- src/modules/users/lab.nix
+++ src/modules/users/lab.nix
@@ -34,6 +34,7 @@
   den.hosts.x86_64-linux.da-vcx-1.users.lab = { };
   den.hosts.x86_64-linux.da-vcx-2.users.lab = { };
   den.hosts.x86_64-linux.da-vcx-3.users.lab = { };
+  den.hosts.x86_64-linux.da-gr75.users.lab = { };
 
   den.hosts.x86_64-linux.en-t65-1.users.lab = { };
 }
