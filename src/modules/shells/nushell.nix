acdee1a26541528aefc1494440d9f1a1b7af2375
John Muchovej <jmuchovej@users.noreply.github.com>
2026-05-16 07:56:33 -0400

    feat(shells): add shells to `environment.shells`

--- src/modules/shells/nushell.nix
+++ src/modules/shells/nushell.nix
@@ -1,14 +1,19 @@
 _: {
-  rbn.shells._.nushell.homeManager =
-    { config, lib, ... }:
-    let
-      inherit (lib) filterAttrs;
-      inherit (lib.strings) hasInfix;
-    in
-    {
-      programs.nushell = {
-        enable = true;
-        shellAliases = filterAttrs (_k: v: !hasInfix " && " v) config.home.shellAliases;
-      };
+  rbn.shells._.nushell = {
+    os = { pkgs, ... }: {
+      environment.shells = [ pkgs.zsh ];
     };
+    homeManager =
+      { config, lib, ... }:
+      let
+        inherit (lib) filterAttrs;
+        inherit (lib.strings) hasInfix;
+      in
+      {
+        programs.nushell = {
+          enable = true;
+          shellAliases = filterAttrs (_k: v: !hasInfix " && " v) config.home.shellAliases;
+        };
+      };
+  };
 }
