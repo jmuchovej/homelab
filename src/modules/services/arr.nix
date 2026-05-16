0dd58f9c0e01a19f5d6d6331cfbc5ea08d9648fa
John Muchovej <jmuchovej@users.noreply.github.com>
2026-05-16 07:56:33 -0400

    refactor(services): clean-up arr refs

--- src/modules/services/arr.nix
+++ src/modules/services/arr.nix
@@ -1,4 +1,43 @@
-{ __findFile, inputs, ... }:
+{
+  lib,
+  __findFile,
+  inputs,
+  ...
+}:
+let
+  arrs =
+    map
+      (
+        arr:
+        arr
+        // {
+          name = lib.strings.toLower arr.proper-name;
+        }
+      )
+      [
+        {
+          proper-name = "Radarr";
+          port = 7878;
+        }
+        {
+          proper-name = "Sonarr";
+          port = 8989;
+        }
+        {
+          proper-name = "Lidarr";
+          port = 8686;
+        }
+        {
+          proper-name = "Readarr";
+          port = 8787;
+          dbs = [ "readarr-cache" ];
+        }
+        {
+          proper-name = "Prowlarr";
+          port = 9696;
+        }
+      ];
+in
 {
   rbn.services._.arr = {
     nixos =
@@ -9,28 +48,19 @@
         ...
       }:
       let
+        inherit (lib) mkMerge mkAfter concatMapStringsSep;
         inherit (host) datacenter;
         sops-file = kind: "${inputs.self}/secrets/${kind}.sops.yaml";
 
-        mk-db-grants =
+        mk-psql =
           {
             name,
             dbs ? [ ],
-          }:
-          lib.concatMapStringsSep "\n" (
-            db: ''psql -tAc 'GRANT ALL ON SCHEMA public TO "${name}"' -d ${db}''
-          ) dbs;
-
-        mk-arr =
-          {
-            proper-name,
-            name ? lib.strings.toLower proper-name,
-            port,
-            dbs ? [ ],
+            ...
           }:
           let
             maindb = name;
-            logdb = "${name}-log";
+            logdb = "${maindb}-log";
             databases = [
               maindb
               logdb
@@ -38,72 +68,59 @@
             ++ dbs;
           in
           {
-            psql = mk-db-grants {
-              inherit name;
-              dbs = databases;
-            };
-
-            config = lib.mkMerge [
-              {
-                sops.secrets."${name}/key".sopsFile = sops-file datacenter;
-
-                sops.templates."${name}/env".content = ''
-                  ${lib.toUpper name}__POSTGRES__APIKEY=${config.sops.placeholder."${name}/key"}
-                '';
-
-                services.postgresql.ensureDatabases = databases;
-                services.postgresql.ensureUsers = [
-                  {
-                    inherit name;
-                    ensureDBOwnership = true;
-                  }
-                ];
-
-                services.${name} = {
-                  enable = true;
-                  openFirewall = true;
-                  environmentFiles = [ config.sops.templates."${name}/env".path ];
-                  settings = {
-                    auth.method = "External";
-                    postgres.user = name;
-                    postgres.host = "localhost";
-                    postgres.maindb = maindb;
-                    postgres.logdb = logdb;
-                  };
-                };
-              }
-            ];
+            inherit databases maindb logdb;
+            sql = concatMapStringsSep "\n" (
+              db: ''psql -tAc 'GRANT ALL ON SCHEMA public TO "${name}"' -d ${db}''
+            ) databases;
           };
 
-        arrs = map mk-arr [
+        mk-arr =
           {
-            port = 7878;
-            proper-name = "Radarr";
-          }
-          {
-            port = 8989;
-            proper-name = "Sonarr";
-          }
-          {
-            port = 8686;
-            proper-name = "Lidarr";
-          }
-          {
-            port = 8787;
-            proper-name = "Readarr";
-            dbs = [ "readarr-cache" ];
-          }
-          {
-            port = 9696;
-            proper-name = "Prowlarr";
-          }
-        ];
+            name,
+            dbs ? [ ],
+            ...
+          }:
+          let
+            psql = mk-psql {
+              inherit name dbs;
+            };
+          in
+          mkMerge [
+            {
+              sops.secrets."${name}/key".sopsFile = sops-file datacenter;
+
+              sops.templates."${name}/env".content = ''
+                ${lib.toUpper name}__POSTGRES__APIKEY=${config.sops.placeholder."${name}/key"}
+              '';
+
+              services.postgresql.ensureDatabases = psql.databases;
+              services.postgresql.ensureUsers = [
+                {
+                  inherit name;
+                  ensureDBOwnership = true;
+                }
+              ];
+
+              services.${name} = {
+                enable = true;
+                openFirewall = true;
+                environmentFiles = [ config.sops.templates."${name}/env".path ];
+                settings = {
+                  auth.method = "External";
+                  postgres.user = name;
+                  postgres.host = "localhost";
+                  postgres.maindb = psql.maindb;
+                  postgres.logdb = psql.logdb;
+                };
+              };
+            }
+          ];
       in
       lib.mkMerge (
         [
           {
-            systemd.services.postgresql.postStart = lib.mkAfter (
-              lib.concatMapStringsSep "\n" (arr: arr.psql) arrs
+            systemd.services.postgresql.postStart = mkAfter (
+              concatMapStringsSep "\n" (arr: (mk-psql arr).sql) arrs
             );
           }
           {
@@ -118,18 +135,24 @@
             };
           }
         ]
-        ++ (map (arr: arr.config) arrs)
+        ++ (map mk-arr arrs)
       );
 
     includes =
       let
         mk-arr-mesh =
-          { name, port }:
+          {
+            name,
+            proper-name,
+            port,
+            ...
+          }:
           (<rbn/mesh/register> {
             inherit name port;
             authed = true;
             healthcheck = "/ping/";
             authentik = {
+              name = proper-name;
               type = "proxy";
               group = "Media Management";
               access = [ "media-managers" ];
@@ -141,27 +164,6 @@
             };
           });
       in
-      [
-        (mk-arr-mesh {
-          name = "radarr";
-          port = 7878;
-        })
-        (mk-arr-mesh {
-          name = "sonarr";
-          port = 8989;
-        })
-        (mk-arr-mesh {
-          name = "lidarr";
-          port = 8686;
-        })
-        (mk-arr-mesh {
-          name = "readarr";
-          port = 8787;
-        })
-        (mk-arr-mesh {
-          name = "prowlarr";
-          port = 9696;
-        })
-      ];
+      map mk-arr-mesh arrs;
   };
 }
