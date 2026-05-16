6399ef1ead4ab7c93feb7b76dff677a726f9ace8
John Muchovej <jmuchovej@users.noreply.github.com>
2026-05-16 07:56:33 -0400

    refactor(ssh): apply `SSH_AUTH_SOCK` on darwin only; concretize `ssh` config

--- src/modules/programs/security/security.nix
+++ src/modules/programs/security/security.nix
@@ -29,16 +29,19 @@
             pkgs,
             ...
           }:
+          let
+            inherit (pkgs.stdenv) isDarwin;
+          in
           {
             home.packages = [ pkgs._1password-cli ];
 
             # Use 1Password as the SSH agent everywhere.
-            home.sessionVariables.SSH_AUTH_SOCK = "$HOME/.1password/agent.sock";
+            home.sessionVariables.SSH_AUTH_SOCK = lib.mkIf isDarwin "$HOME/.1password/agent.sock";
 
             # macOS puts the agent socket under Group Containers; symlink it
             # to ~/.1password/agent.sock so the same SSH_AUTH_SOCK works on
             # both platforms. Linux's 1Password writes there directly.
-            home.file.".1password/agent.sock" = lib.mkIf pkgs.stdenv.isDarwin {
+            home.file.".1password/agent.sock" = lib.mkIf isDarwin {
               source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
             };
           };
