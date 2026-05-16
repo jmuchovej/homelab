9fa4bf727c451f0ff6b0b802cd8116fbcc674064
John Muchovej <jmuchovej@users.noreply.github.com>
2026-05-16 07:56:33 -0400

    refactor(services/postgres): allow passworded-TCP connections to postgres

--- src/modules/services/postgres.nix
+++ src/modules/services/postgres.nix
@@ -1,5 +1,4 @@
-{ inputs, ... }:
-{
+_: {
   rbn.services._.postgres.nixos =
     {
       config,
@@ -7,58 +6,67 @@
       pkgs,
       ...
     }:
-    {
-      sops.secrets."postgres/terraform".sopsFile = "${inputs.self}/secrets/secrets.sops.yaml";
+    let
+      inherit (lib.rbn) get-secret';
+    in
+    lib.mkMerge [
+      (get-secret' "postgres/terraform")
+      {
 
-      sops.templates."init.sql" = {
-        content = ''
-          CREATE USER terraform WITH PASSWORD '${config.sops.placeholder."postgres/terraform"}';
-          CREATE DATABASE terraform;
-          GRANT ALL PRIVILEGES ON DATABASE terraform TO terraform;
-          ALTER DATABASE terraform OWNER TO terraform;
-          \c terraform;
-          GRANT ALL ON SCHEMA public TO terraform;
-          GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO terraform;
-        '';
-        owner = "postgres";
-      };
-
-      services.postgresql = {
-        enable = true;
-        package = pkgs.postgresql_17;
-        authentication = pkgs.lib.mkOverride 10 ''
-          # type database DBuser  origin-address auth-method
-          local  all      all                    trust
-          # ipv4
-          host   all      all     127.0.0.1/32   trust
-          # ipv6
-          host   all      all     ::1/128        trust
-        '';
-        initialScript = config.sops.templates."init.sql".path;
-        initdbArgs = [
-          "--locale-provider=builtin"
-          "--builtin-locale=C.UTF-8"
-          "--encoding=UTF8"
-        ];
-      };
-
-      services.postgresqlBackup = {
-        enable = true;
-        backupAll = true;
-        startAt = "*-*-* 10:00:00";
-      };
-
-      services.traefik.staticConfigOptions.entryPoints.postgres.address = "0.0.0.0:5433";
-
-      services.traefik.dynamicConfigOptions.tcp = {
-        services.postgres.loadBalancer.servers = [
-          { address = "127.0.0.1:5432"; }
-        ];
-        routers.postgres = {
-          entryPoints = [ "postgres" ];
-          rule = "HostSNI(`*`)";
-          service = "postgres";
+        sops.templates."init.sql" = {
+          content = ''
+            CREATE USER terraform WITH PASSWORD '${config.sops.placeholder."postgres/terraform"}';
+            CREATE DATABASE terraform;
+            GRANT ALL PRIVILEGES ON DATABASE terraform TO terraform;
+            ALTER DATABASE terraform OWNER TO terraform;
+            \c terraform;
+            GRANT ALL ON SCHEMA public TO terraform;
+            GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO terraform;
+          '';
+          owner = "postgres";
         };
-      };
-    };
+
+        services.postgresql = {
+          enable = true;
+          enableTCPIP = true;
+          package = pkgs.postgresql_17;
+          authentication = pkgs.lib.mkOverride 10 ''
+            # type database DBuser  origin-address auth-method
+            local  all      all                    trust
+            # ipv4
+            host   all      all     127.0.0.1/32   trust
+            host   all      all     10.244.0.0/16  scram-sha-256
+            # ipv6
+            host   all      all     ::1/128        trust
+          '';
+          initialScript = config.sops.templates."init.sql".path;
+          initdbArgs = [
+            "--locale-provider=builtin"
+            "--builtin-locale=C.UTF-8"
+            "--encoding=UTF8"
+          ];
+        };
+
+        networking.firewall.allowedTCPPorts = [ 5432 ];
+
+        services.postgresqlBackup = {
+          enable = true;
+          backupAll = true;
+          startAt = "*-*-* 10:00:00";
+        };
+
+        services.traefik.staticConfigOptions.entryPoints.postgres.address = "0.0.0.0:5433";
+
+        services.traefik.dynamicConfigOptions.tcp = {
+          services.postgres.loadBalancer.servers = [
+            { address = "127.0.0.1:5432"; }
+          ];
+          routers.postgres = {
+            entryPoints = [ "postgres" ];
+            rule = "HostSNI(`*`)";
+            service = "postgres";
+          };
+        };
+      }
+    ];
 }
