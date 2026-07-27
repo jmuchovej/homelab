dd6ec366008c6b7802958b8c10bd48b133fa2ad1
John Muchovej <jmuchovej@users.noreply.github.com>
2026-07-27 08:40:48 -0400

    feat(nix/programs/toolchains/development): add `closure-size`

--- src/modules/programs/toolchains/development.nix
+++ src/modules/programs/toolchains/development.nix
@@ -6,9 +6,6 @@
       config,
       ...
     }:
-    let
-      nr-bin = lib.getExe pkgs.nixpkgs-review;
-    in
     {
       home.packages = with pkgs; [
         devenv
@@ -18,6 +15,20 @@
         tokei
         onefetch
         act
+
+        nix-tree
+        nix-du
+        graphviz
+
+        (writeShellScriptBin "closure-size" ''
+          nix path-info --recursive --closure-size --human-readable \
+            "''${1:-/run/current-system}" | sort --human-numeric-sort --key=2
+        '')
+
+        (writeShellScriptBin "store-size" ''
+          nix path-info --recursive --size --human-readable \
+            "''${1:-/run/current-system}" | sort --human-numeric-sort --key=2
+        '')
       ];
 
       programs.direnv = {
@@ -29,18 +40,22 @@
 
       # environment.sessionVariables.DIRENV_LOG_FORMAT = ""; # Blank so direnv will shut up
 
-      home.shellAliases = {
-        prefetch-sri = "nix store prefetch-file $1";
-        nrh = "${nr-bin} rev HEAD";
-        nra = ''${nr-bin} pr $1 --systems "all"'';
-        nrap = ''${nr-bin} pr $1 --systems "all" --post-result --num-parallel-evals 4'';
-        nrd = ''${nr-bin} pr $1 --systems "x86_64-darwin aarch64-darwin" --num-parallel-evals 2'';
-        nrdp = ''${nr-bin} pr $1 --systems "x86_64-darwin aarch64-darwin" --num-parallel-evals 2 --post-result'';
-        nrl = ''${nr-bin} pr $1 --systems "x86_64-linux aarch64-linux" --num-parallel-evals 2'';
-        nrlp = ''${nr-bin} pr $1 --systems "x86_64-linux aarch64-linux" --num-parallel-evals 2 --post-result'';
-        nrmp = ''${nr-bin} pr $1 --systems "x86_64-darwin aarch64-darwin aarch64-linux" --num-parallel-evals 3 --post-result'';
-        nup = "nix-shell maintainers/scripts/update.nix --argstr package $1";
-        num = "nix-shell maintainers/scripts/update.nix --argstr maintainer $1";
-      };
+      home.shellAliases =
+        let
+          nr-bin = lib.getExe pkgs.nixpkgs-review;
+        in
+        {
+          prefetch-sri = "nix store prefetch-file $1";
+          nrh = "${nr-bin} rev HEAD";
+          nra = ''${nr-bin} pr $1 --systems "all"'';
+          nrap = ''${nr-bin} pr $1 --systems "all" --post-result --num-parallel-evals 4'';
+          nrd = ''${nr-bin} pr $1 --systems "x86_64-darwin aarch64-darwin" --num-parallel-evals 2'';
+          nrdp = ''${nr-bin} pr $1 --systems "x86_64-darwin aarch64-darwin" --num-parallel-evals 2 --post-result'';
+          nrl = ''${nr-bin} pr $1 --systems "x86_64-linux aarch64-linux" --num-parallel-evals 2'';
+          nrlp = ''${nr-bin} pr $1 --systems "x86_64-linux aarch64-linux" --num-parallel-evals 2 --post-result'';
+          nrmp = ''${nr-bin} pr $1 --systems "x86_64-darwin aarch64-darwin aarch64-linux" --num-parallel-evals 3 --post-result'';
+          nup = "nix-shell maintainers/scripts/update.nix --argstr package $1";
+          num = "nix-shell maintainers/scripts/update.nix --argstr maintainer $1";
+        };
     };
 }
