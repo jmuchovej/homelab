5437d9116425d463797510dc62bede09205bcb54
John Muchovej <jmuchovej@users.noreply.github.com>
2026-07-27 08:40:48 -0400

    chore(nix/editors/zed): update `zed` settings

--- src/modules/programs/editors/_zed/themes.nix
+++ src/modules/programs/editors/_zed/themes.nix
@@ -1,17 +1,32 @@
 { lib, ... }:
 {
+  extensions = [
+    "catppuccin"
+    "catppuccin-blur"
+    "catppuccin-icons"
+    "charmed-icons"
+    "vscode-icons"
+    "colored-zed-icons-theme"
+    "jetbrains-new-ui-icons"
+    "material-icon-theme"
+    "nvim-nightfox"
+  ];
   settings = lib.mkMerge [
     {
       # Theme Settings
       # https://zed.dev/docs/visual-customization#themes
       theme = {
-        dark = "Catppuccin Frappé";
-        light = "Catppuccin Latte";
+        # dark = "Catppuccin Frappé";
+        light = "Dawnfox - opaque";
+        # light = "Catppuccin Latte";
+        dark = "Carbonfox - opaque";
         mode = "system";
       };
       icon_theme = {
-        dark = "Catppuccin Frappé";
-        light = "Catppuccin Latte";
+        # dark = "Catppuccin Frappé";
+        dark = "Soft Charmed Icons";
+        # light = "Catppuccin Latte";
+        light = "Light Charmed Icons";
         mode = "system";
       };
     }
