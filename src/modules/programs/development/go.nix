27709e600c67ad4d3f2715563a6bb18e551743ab
John Muchovej <jmuchovej@users.noreply.github.com>
2026-07-27 08:40:48 -0400

    refactor(nix/programs/development): squash vscode/zed settings into `programs.{...}` blocks

--- src/modules/programs/development/go.nix
+++ src/modules/programs/development/go.nix
@@ -1,65 +1,47 @@
-_: {
-  rbn.programs._.development._.go.homeManager =
-    {
-      pkgs,
-      lib,
-      config,
-      ...
-    }:
-    let
-      inherit (lib) mkIf;
+{
+  rbn.programs._.development._.go.homeManager = { pkgs, ... }: {
+    home.packages = with pkgs; [
+      go
+      air
+      golangci-lint
+      gotestsum
+      gopls
+    ];
 
-      vsc-extensions = with pkgs.open-vsx; [
+    programs.vscode = {
+      profiles.default.extensions = with pkgs.open-vsx; [
         golang.go
       ];
-      vsc-user-settings = { };
+      profiles.default.userSettings = { };
+    };
 
+    programs.zed-editor = {
       # https://zed.dev/docs/languages/go
-      zed = {
-        extensions = [ ];
-        extraPackages = with pkgs; [
-          gopls
-          golangci-lint
-          gotestsum
-        ];
-        userSettings = {
-          lsp.gopls = {
-            initialization_options = {
-              hints = {
-                assignVariableTypes = true;
-                compositeLiteralFields = true;
-                compositeLiteralTypes = true;
-                constantValues = true;
-                functionTypeParameters = true;
-                parameterNames = true;
-                rangeVariableTypes = true;
-              };
-            };
-          };
-          languages.Go = {
-            tab_size = 2;
-            language_servers = [ "gopls" ];
-          };
-        };
-      };
-
-    in
-    {
-      home.packages = with pkgs; [
-        go
-        air
+      extensions = [ ];
+      extraPackages = with pkgs; [
+        gopls
         golangci-lint
         gotestsum
-        gopls
       ];
-
-      programs.vscode = mkIf (config.programs.vscode.enable or false) {
-        profiles.default.extensions = vsc-extensions;
-        profiles.default.userSettings = vsc-user-settings;
-      };
-
-      programs.zed-editor = mkIf (config.programs.zed-editor.enable or false) {
-        inherit (zed) extensions extraPackages userSettings;
+      userSettings = {
+        lsp.gopls = {
+          initialization_options = {
+            hints = {
+              assignVariableTypes = true;
+              compositeLiteralFields = true;
+              compositeLiteralTypes = true;
+              constantValues = true;
+              functionTypeParameters = true;
+              parameterNames = true;
+              rangeVariableTypes = true;
+            };
+          };
+        };
+        languages.Go = {
+          tab_size = 2;
+          language_servers = [ "gopls" ];
+        };
       };
     };
+  };
 }
--- src/modules/programs/development/nix.nix
+++ src/modules/programs/development/nix.nix
@@ -1,70 +1,53 @@
-_: {
-  rbn.programs._.development._.nix.homeManager =
-    {
-      pkgs,
-      lib,
-      config,
-      ...
-    }:
-    let
-      inherit (lib) mkIf;
+{
+  rbn.programs._.development._.nix.homeManager = { pkgs, ... }: {
+    home.packages = with pkgs; [
+      nixd
+      nil
+      nixfmt
+      nix-prefetch-git
+      hydra-check
+      nixpkgs-hammering
+      nixpkgs-lint-community
+      nixpkgs-review
+      nix-update
+      nix-output-monitor
+      alejandra
+    ];
 
-      vsc-extensions = with pkgs.open-vsx; [
+    programs.vscode = {
+      profiles.default.extensions = with pkgs.open-vsx; [
         jnoortheen.nix-ide
         # arrterian.nix-env-selector
       ];
-      vsc-user-settings = { };
+      profiles.default.userSettings = { };
+    };
 
-      zed = {
-        # https://github.com/zed-extensions/nix
-        extensions = [ "nix" ];
-        extraPackages = with pkgs; [
-          nixd
-          nixfmt
-          nix-output-monitor
-        ];
-        userSettings = {
-          languages.Nix = {
-            tab_size = 2;
-            language_servers = [
-              "nixd"
-              "!nil"
+    programs.zed-editor = {
+      # https://github.com/zed-extensions/nix
+      extensions = [ "nix" ];
+      extraPackages = with pkgs; [
+        nixd
+        nixfmt
+        nix-output-monitor
+      ];
+      userSettings = {
+        languages.Nix = {
+          tab_size = 2;
+          language_servers = [
+            "nixd"
+            "!nil"
+          ];
+        };
+        lsp.nixd = {
+          initialization_options = {
+            formatting.command = [
+              "nixfmt"
+              "--quiet"
+              "--"
             ];
-          };
-          lsp.nixd = {
-            initialization_options = {
-              formatting.command = [
-                "nixfmt"
-                "--quiet"
-                "--"
-              ];
-            };
           };
         };
       };
-    in
-    {
-      home.packages = with pkgs; [
-        nixd
-        nil
-        nixfmt
-        nix-prefetch-git
-        hydra-check
-        nixpkgs-hammering
-        nixpkgs-lint-community
-        nixpkgs-review
-        nix-update
-        nix-output-monitor
-        alejandra
-      ];
-
-      programs.vscode = mkIf (config.programs.vscode.enable or false) {
-        profiles.default.extensions = vsc-extensions;
-        profiles.default.userSettings = vsc-user-settings;
-      };
-
-      programs.zed-editor = mkIf (config.programs.zed-editor.enable or false) {
-        inherit (zed) extensions extraPackages userSettings;
-      };
     };
+  };
 }
