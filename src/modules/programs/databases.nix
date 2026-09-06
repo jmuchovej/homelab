1dbde2873c5a8d51b78f3250abfedb82461a7da8
John Muchovej <jmuchovej@users.noreply.github.com>
2026-09-06 12:39:35 -0400

    fix(nix/programs/databases): allow `beekeeper-studio` w/ insecure electron

--- src/modules/programs/databases.nix
+++ src/modules/programs/databases.nix
@@ -1,6 +1,10 @@
-{
+{ den, ... }: {
   rbn.programs._.databases = {
     _.beekeeper = {
+      includes = [
+        (den.batteries.insecure [ "beekeeper-studio-6.0.5" ])
+      ];
+
       hm = { pkgs, ... }: {
         home.packages = [ pkgs.beekeeper-studio ];
       };
