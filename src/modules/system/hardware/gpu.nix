9cda3055440eba7de4332d24af95ebb69ad9290e
John Muchovej <5000729+jmuchovej@users.noreply.github.com>
2026-09-07 12:33:32 -0400

    fix(nix/system/hardware/gpu): update `nixos-cuda` cache URL/key
    Signed-off-by: John Muchovej <5000729+jmuchovej@users.noreply.github.com>

--- src/modules/system/hardware/gpu.nix
+++ src/modules/system/hardware/gpu.nix
@@ -50,12 +50,8 @@
           nixpkgs.config.nvidia.acceptLicense = true;
 
           nix.settings = {
-            substituters = [
-              "https://cuda-maintainers.cachix.org"
-            ];
-            trusted-public-keys = [
-              "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-            ];
+            substituters = [ "https://cache.nixos-cuda.org" ];
+            trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
           };
 
           services.xserver.videoDrivers = lib.mkDefault [ "nvidia" ];
